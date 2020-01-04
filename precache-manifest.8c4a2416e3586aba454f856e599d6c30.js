d993c1b982ccd85b3ef8162d956aa1f3e6aab1a6
rahat14 <rahatshovo6666@gmail.com>
2020-01-05 03:29:33 +0600

    Updates

---
+++ precache-manifest.8c4a2416e3586aba454f856e599d6c30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f7e57eb2dd419d09186",
+    "url": "/static/js/main.f6902480.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9f7bc571661205fe9d1",
+    "url": "/static/js/2.ce8a9bb8.chunk.js"
+  },
+  {
+    "revision": "0c075813bddcc2033b671123ffec4558",
+    "url": "/index.html"
+  }
+];
--- src/components/elements/MenuItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function MenuItem(props) {
-    return(
-        <a href={props.href} className="navbar-item is-unselectable">{props.text}</a>
-    )
-}
-
-export default MenuItem
