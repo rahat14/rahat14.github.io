095be82d02f372c326737935a4e17c564d6a3fe4
rahat14 <rahatshovo6666@gmail.com>
2020-06-20 19:14:54 +0600

    f

--- src/components/sections/Articles.js
+++ src/components/sections/Articles.js
@@ -60,7 +60,56 @@ class Projects extends Component{
                     <h1 className="title">Projects</h1>
                     <h2 className="subtitle is-4">My latest Projects...</h2>
                     <div className="columns">
+
+
+                        <div className="card">
+                            <div className="card-header">
+                                <a href="https://play.google.com/store/apps/details?id=com.syntex.stories" target="blank"><p className="card-header-title">Vinci</p></a>
+                                <a href="https://play.google.com/store/apps/details?id=com.syntex.stories" target="blank" className="card-header-icon" aria-label="Play Store">
+                                    <span className="icon">
+                                        <i className="fab fa-2x fa-google-play"></i>
+                                    </span>
+                                </a>
+                            </div>
+                            <div className="card-image">
+                                <figure className="cardimage">
+                                    <img width="1000" height="420" src="https://lh3.googleusercontent.com/Our-O4XmkgTAgcoiPYt4TgLsYXi7L8SZDHYiW0-6WqmTh1MrZtJG_AThewdFr2hPAbU=s180-rw" alt="" />
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <h1 className="heading" id="stack">Java,Mysql,Onesignal</h1>
+                                <div className="content">
+                                    <p>An Soical App For Newton Iot Company. </p>
+                                </div>
+                                <a href="https://play.google.com/store/apps/details?id=com.syntex.stories" target="blank">Check This Project</a>
+                            </div>
+                        </div>
+
+
                       
+                        <div className="card">
+                            <div className="card-header">
+                                <a href="https://play.google.com/store/apps/details?id=com.metacodersbd.safeguard" target="blank"><p className="card-header-title">BakSoinik</p></a>
+                                <a href="https://play.google.com/store/apps/details?id=com.metacodersbd.safeguard" target="blank" className="card-header-icon" aria-label="Play Store">
+                                    <span className="icon">
+                                        <i className="fab fa-2x fa-google-play"></i>
+                                    </span>
+                                </a>
+                            </div>
+                            <div className="card-image">
+                                <figure className="cardimage">
+                                    <img width="1000" height="420" src="https://lh3.googleusercontent.com/oF3Bb8JeIpadDmOWesJdeq_OuGilBJ7e6rrVqHTvGImTQUjZXUJUHH7r-eUHoogUJQ=s180-rw" alt="" />
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <h1 className="heading" id="stack">Java,Mysql,Onesignal,Map</h1>
+                                <div className="content">
+                                    <p>An Emergency Response App Built For A Ngo</p>
+                                </div>
+                                <a href="https://play.google.com/store/apps/details?id=com.metacodersbd.safeguard" target="blank">Check This Project</a>
+                            </div>
+                        </div>
+
 
                         <div className="card"  >
                             <div className="card-header">
@@ -84,13 +133,15 @@ class Projects extends Component{
                             </figure>
                         </div>
                         <div className="card-content">
-                            <h1 className="heading">Java,Firebase,Onesignal</h1>
+                                <h1 className="heading" id="stack">Java,Firebase,Onesignal</h1>
                             <div className="content">
-                                <p>A Food Delivery Start-Up in Cumilla </p>
+                                <p>A Food Delivery App in Cumilla.</p>
                             </div>
-                            <a href="https://play.google.com/store/apps/details?id=com.metacodersbd.hungrybirds" target="blank">Check This Project</a>
+
+                            <a   className = "hb" href="https://play.google.com/store/apps/details?id=com.metacodersbd.hungrybirds" target="blank">Check This Project</a>
                         </div>
                     </div>
+
 
                         <div className="card">
                             <div className="card-header">
@@ -107,14 +158,24 @@ class Projects extends Component{
                                 </figure>
                             </div>
                             <div className="card-content">
-                                <h1 className="heading">Java,RestApi,Php</h1>
+                                <h1 className="heading" id="stack">Java,RestApi,Php</h1>
                                 <div className="content">
-                                    <p>A Streaming For An Isp </p>
+                                    <p>A Media Streaming App For An Isp. </p>
                                 </div>
                                 <a href="https://play.google.com/store/apps/details?id=com.ksbnet.official" target="blank">Check This Project</a>
                             </div>
                         </div>
+
+
+                     
+                    </div>
+
+                    <div className="columns">
+
+
                 
+                      
+
 
                         <div className="card">
                             <div className="card-header">
@@ -131,9 +192,9 @@ class Projects extends Component{
                                 </figure>
                             </div>
                             <div className="card-content">
-                                <h1 className="heading">Java,Firebase,Onesignal</h1>
+                                <h1 className="heading" id="stack">Java,Firebase,Onesignal</h1>
                                 <div className="content">
-                                    <p>An App For Job Seeker </p>
+                                    <p>An App For Job Seekers in Bangladesh. </p>
                                 </div>
                                 <a href="https://play.google.com/store/apps/details?id=com.metacoders.home" target="blank">Check This Project</a>
                             </div>
@@ -158,17 +219,18 @@ class Projects extends Component{
                                 </figure>
                             </div>
                             <div className="card-content">
-                                <h1 className="heading">Java,RestApi,Gps,Map</h1>
+                                <h1 className="heading" id="stack">Java,RestApi,Gps,ButterKnife</h1>
                                 <div className="content">
-                                    <p>An Islamic Companion </p>
+                                    <p>An Islamic Companion For Muslim BotherHood </p>
                                 </div>
-                                <a href="https://play.google.com/store/apps/details?id=com.metacoders.home" target="blank">Check The Project</a>
+                                <a href="https://play.google.com/store/apps/details?id=org.sheba24.beauty.islam.prayer.time.quran.hadith" target="blank">Check The Project</a>
                             </div>
                         </div>
 
 
-
                     </div>
+
+                    
                 </div>
             </section>
         )
